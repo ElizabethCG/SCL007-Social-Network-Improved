281edf23995885254bcb17ce55059d9161d18bb1
ElizabethCG <elizabeth.caceres@gmail.com>
2019-03-31 19:49:03 -0300

    Instala Babel y otras dependencias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,45 @@
-module.exports = {
-"extends": "airbnb-base"
+// module.exports = {
+// "extends": "airbnb-base"
+//
+//
+//     // "env": {
+//     //     "browser": true,
+//     //     "commonjs": true,
+//     //     "es6": true
+//     // },
+//     // "extends": "eslint:recommended",
+//     // "parserOptions": {
+//     //     "ecmaVersion": 2017,
+//     //     "sourceType": "module"
+//     // },
+//     // "rules": {
+//     //     "indent": [
+//     //         "error",
+//     //         "tab"
+//     //     ],
+//     //     "linebreak-style": [
+//     //         "error",
+//     //         "unix"
+//     //     ],
+//     //     "quotes": [
+//     //         "error",
+//     //         "single"
+//     //     ],
+//     //     "semi": [
+//     //         "error",
+//     //         "always"
+//     //     ]
+//     // }
+// };
 
-
-    // "env": {
-    //     "browser": true,
-    //     "commonjs": true,
-    //     "es6": true
-    // },
-    // "extends": "eslint:recommended",
-    // "parserOptions": {
-    //     "ecmaVersion": 2017,
-    //     "sourceType": "module"
-    // },
-    // "rules": {
-    //     "indent": [
-    //         "error",
-    //         "tab"
-    //     ],
-    //     "linebreak-style": [
-    //         "error",
-    //         "unix"
-    //     ],
-    //     "quotes": [
-    //         "error",
-    //         "single"
-    //     ],
-    //     "semi": [
-    //         "error",
-    //         "always"
-    //     ]
-    // }
-};
+{
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  }
+}
